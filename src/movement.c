d1c90d12cd7d86a72b40a7b73a2aa5c65ac5d69f
Geq0nek <93310012+Geq0nek@users.noreply.github.com>
2025-08-18 19:29:51 +0200

    Update movement.c

--- src/movement.c
+++ src/movement.c
@@ -1,93 +1,40 @@
 #include "movement.h"
 
-// SERVO 1
-// uint16_t pwm_value = (20 * (4096 / 180)); - 180* GÓRA - 455,(1) ~ 455
-// uint16_t pwm_value = (57 * (4096 / 180)); - 90* SRODEK  - 1297,0(6) ~ 1297
-// uint16_t pwm_value = (94 * (4096 / 180)); - 0* DÓŁ - 2139,0(2) ~ 2139
-// 
-
-// SERVO 2
-// uint16_t pwm_value = (20 * (4096 / 180)); - 0* DÓŁ - 455,(1) ~ 455
-// uint16_t pwm_value = (57 * (4096 / 180)); - 90* SRODEK - 1297,0(6) ~ 1297
-// uint16_t pwm_value = (94 * (4096 / 180)); - 180* GÓRA  - 2139,0(2) ~ 2139
-
-long map(long x)
-{
-    return (x - 0) * (2139 - 455) / (180 - 0) + 455;
+static inline uint16_t angle_to_pwm(uint16_t angle) {
+    return (angle * (PWM_DOWN - PWM_UP) / 180) + PWM_UP;
 }
 
-void move_forward(void)
-{
-    uint16_t pwm_value = (57 * (4096 / 180));// 90*
-    ESP_ERROR_CHECK(pca9685_set_pwm(4, 0, pwm_value));
-
-    ESP_LOGI(MOVE_TAG, "Moving forward");
+static void move(const char* direction, uint8_t channel, uint16_t angle) {
+    uint16_t pwm = angle_to_pwm(angle);
+    ESP_ERROR_CHECK(pca9685_set_pwm(channel, 0, pwm));
+    ESP_LOGI(MOVE_TAG, "Moving %s", direction);
 }
 
-void move_backward(void)
-{
 
-    uint16_t pwm_value = (94 * (4096 / 180)); // 0*
-    ESP_ERROR_CHECK(pca9685_set_pwm(4, 0, pwm_value));
+void move_forward(void)  { move("forward", 4, 90); }
+void move_backward(void) { move("backward", 4, 0); }
+void move_left(void)     { move("left", 4, 66); }
+void move_right(void)    { move("right", 4, 180); }
+void stop_moving(void)   { ESP_LOGI(MOVE_TAG, "Stop"); }
 
-    ESP_LOGI(MOVE_TAG, "Moving backward");
+void stand(void) {
+    struct { uint8_t channel; uint16_t angle; } cmds[] = {
+        {1, 130}, {4, 50}, {7, 130}, {10, 50},
+        {0, 50},  {3, 130}, {6, 50},  {9, 130}
+    };
+
+    for(int i = 0; i < 8; i++) {
+        pca9685_set_pwm(cmds[i].channel, 0, angle_to_pwm(cmds[i].angle));
+    }
 }
 
-void move_left(void)
-{
-    uint16_t pwm_value = (66 * (4096 / 180));
-    ESP_ERROR_CHECK(pca9685_set_pwm(4, 0, pwm_value));
-    ESP_LOGI(MOVE_TAG, "Moving left");
-}
+void sit(void) {
+    struct { uint8_t channel; uint16_t angle; } cmds[] = {
+        {9, 90}, {6, 90}, {10, 90}, {7, 90},
+        {0, 90}, {3, 90}, {1, 90}, {4, 90}
+    };
 
-void move_right(void)
-{
-    uint16_t pwm_value = (20 * (4096 / 180)); //180*
-    ESP_ERROR_CHECK(pca9685_set_pwm(4, 0, pwm_value));
-    ESP_LOGI(MOVE_TAG, "Moving right");
-}
-
-void stop_moving(void)
-{
-    ESP_LOGI(MOVE_TAG, "Stop");
-}
-
-void stand(void)
-{   
-    pca9685_set_pwm(1, 0, map(130));
-    pca9685_set_pwm(4, 0, map(50));
-    pca9685_set_pwm(7, 0, map(130));   
-    pca9685_set_pwm(10, 0, map(50)); 
-
-    pca9685_set_pwm(0, 0, map(50));
-    pca9685_set_pwm(3, 0, map(130));
-    pca9685_set_pwm(6, 0, map(50));   
-    pca9685_set_pwm(9, 0, map(130));   
-}
-
-void sit(void)
-{
-    pca9685_set_pwm(9, 2139, map(90));
-    pca9685_set_pwm(6, 455, map(90));
-
-
-    pca9685_set_pwm(10, 2139, map(90));
-    pca9685_set_pwm(7, 455, map(90));
-
-    pca9685_set_pwm(0, 455, map(90));
-    pca9685_set_pwm(3, 2139, map(90));;
-
-    pca9685_set_pwm(1, 455, map(90));
-    pca9685_set_pwm(4, 2139, map(90));
-    
-
-    // pca9685_set_pwm(1, 0, map(140));
-    // pca9685_set_pwm(4, 0, map(40));
-    // pca9685_set_pwm(7, 0, map(140));   
-    // pca9685_set_pwm(10, 0, map(40)); 
-
-    // pca9685_set_pwm(0, 0, map(90));
-    // pca9685_set_pwm(3, 0, map(90));
-    // pca9685_set_pwm(6, 0, map(90));   
-    // pca9685_set_pwm(9, 0, map(90));   
+    for(int i = 0; i < 8; i++) {
+        pca9685_set_pwm(cmds[i].channel, 0, angle_to_pwm(cmds[i].angle));
+    }
 }
